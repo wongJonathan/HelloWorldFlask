68d1ab9f266014eeff082075ed8a8848fb928ec9
jwong <jwong@amplify.com>
2017-01-31 08:58:23 -0500

    Fully functional deployable lambda function

--- myService/app.py
+++ myService/app.py
@@ -1,6 +1,7 @@
 import connexion
 from flask import Flask
-app = Flask(__name__)
+
+
 """
 For inputing name
 @app.route('/<name>')
@@ -8,11 +9,13 @@ def post_greeting(name):
     return "hello {}".format(name),200
 """
 
-@app.route('/')
 def post_world():
     return "hello world"
 
+app = connexion.App(__name__, 9090)
+app.add_api('swagger.yaml')
+
+application = app.app
+
 if __name__ == '__main__':
-    app = connexion.App(__name__, 9090)
-    app.add_api('swagger.yaml')
     app.run()
