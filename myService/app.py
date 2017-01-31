ea31b436776ba1ee49cae47f694fef8d79c02409
jwong <jwong@amplify.com>
2017-01-31 10:20:06 -0500

    Changed the url paths

--- myService/app.py
+++ myService/app.py
@@ -1,21 +1,13 @@
 import connexion
-from flask import Flask
 
-
-"""
-For inputing name
-@app.route('/<name>')
-def post_greeting(name):
-    return "hello {}".format(name),200
-"""
+def get_world():
+    return "Hello World Get"
 
 def post_world():
-    return "hello world"
+    return "Hello World Post"
 
 app = connexion.App(__name__, 9090)
 app.add_api('swagger.yaml')
-
-application = app.app
 
 if __name__ == '__main__':
     app.run()
